55667ca2c48c1532a4c546409a483d75b65dacec
Victor Alvarenga <info@victoralvarenga.com>
2022-10-09 14:59:02 -0300

    Add project dependencies

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ echo "installing dependencies"
 apt-get install -y python3
 apt-get install -y python3-pip
 apt-get install -y python3-venv
-# apt-get install -y python3-opencv // FIX Later
+apt-get install -y libgl1
 
 python3 -V
 
@@ -28,9 +28,38 @@ cp /workspace/sd-ui-prebuild/run-ui.ipynb /workspace/run-ui.ipynb
 
 echo "Copied"
 echo "---------------------------------"
-echo "clone stable-diffusion webui"
+echo "Clone stable-diffusion webui"
 cd /workspace
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
+
+echo "Repo Cloned" 
+echo "---------------------------------"
+echo "Add extra dependencies"
+cd stable-diffusion-webui
+
+mkdir repositories
+git clone https://github.com/CompVis/stable-diffusion.git repositories/stable-diffusion
+git clone https://github.com/CompVis/taming-transformers.git repositories/taming-transformers
+git clone https://github.com/sczhou/CodeFormer.git repositories/CodeFormer
+git clone https://github.com/salesforce/BLIP.git repositories/BLIP
+
+pip install transformers==4.19.2 diffusers invisible-watermark --prefer-binary
+
+# install k-diffusion
+pip install git+https://github.com/crowsonkb/k-diffusion.git --prefer-binary
+
+# (optional) install GFPGAN (face restoration)
+pip install git+https://github.com/TencentARC/GFPGAN.git --prefer-binary
+
+# (optional) install requirements for CodeFormer (face restoration)
+pip install -r repositories/CodeFormer/requirements.txt --prefer-binary
+
+# install requirements of web ui
+pip install -r requirements.txt  --prefer-binary
+
+# update numpy to latest version
+pip install -U numpy  --prefer-binary
+
 
 if [[ $PUBLIC_KEY ]]
 then
