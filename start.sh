cc31837eac5b71cf9ad661470204d9470f444910
Victor Alvarenga <info@victoralvarenga.com>
2022-10-09 19:43:21 -0300

    reduce build time

--- start.sh
+++ start.sh
@@ -33,33 +33,6 @@ cd /workspace
 git clone https://github.com/AUTOMATIC1111/stable-diffusion-webui.git
 
 echo "Repo Cloned" 
-echo "---------------------------------"
-echo "Add extra dependencies"
-cd stable-diffusion-webui
-
-mkdir repositories
-git clone https://github.com/CompVis/stable-diffusion.git repositories/stable-diffusion
-git clone https://github.com/CompVis/taming-transformers.git repositories/taming-transformers
-git clone https://github.com/sczhou/CodeFormer.git repositories/CodeFormer
-git clone https://github.com/salesforce/BLIP.git repositories/BLIP
-
-pip install transformers==4.19.2 diffusers invisible-watermark --prefer-binary
-
-# install k-diffusion
-pip install git+https://github.com/crowsonkb/k-diffusion.git --prefer-binary
-
-# (optional) install GFPGAN (face restoration)
-pip install git+https://github.com/TencentARC/GFPGAN.git --prefer-binary
-
-# (optional) install requirements for CodeFormer (face restoration)
-pip install -r repositories/CodeFormer/requirements.txt --prefer-binary
-
-# install requirements of web ui
-pip install -r requirements.txt  --prefer-binary
-
-# update numpy to latest version
-pip install -U numpy  --prefer-binary
-
 
 if [[ $PUBLIC_KEY ]]
 then
